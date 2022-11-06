f34a3b4d1196ce380b348c9639aca57ed1170870
ElmiraOn <elmiraonagh@yahoo.com>
2022-11-06 13:56:11 -0500

    added python files

---
+++ Visualizer/VisualizerApp.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+import streamlit.components.v1 as components
+import networkx as nx
+import matplotlib.pyplot as plt
+from pyvis.network import Network
+import views 
+
+
+
+#### this file contains code for streamlit deployment
+
+st.title('LePa: Learning Path Project')
+st.sidebar.title('Choose your view')
+option=st.sidebar.selectbox('select graph',('whole LePa','view 2', 'view 3'))
+demo = st.checkbox("use demo file")
+
+# physics=st.sidebar.checkbox('add physics interactivity?')
+if demo: 
+    # get demo file from github
+    # url = "https://drive.google.com/file/d/1kWkw57BPSKQ-my37NvHXszcMbuKFw7B0/view?usp=sharing"
+    # res = requests.get(url, allow_redirects=True)
+    # with open('demo.csv','wb') as file:
+    #     file.write(res.content)
+    # from google drive
+    url="https://drive.google.com/file/d/1kWkw57BPSKQ-my37NvHXszcMbuKFw7B0/view?usp=sharing"
+    url='https://drive.google.com/uc?id=' + url.split('/')[-2]
+    df = pd.read_csv(url)
+    df.to_csv("demo.csv")
+
+    uploaded_file = "demo.csv"
+
+else:
+    uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    if option=='whole LePa':
+        views.viewAll(uploaded_file)
+        HtmlFile = open("viewAll.html", 'r', encoding='utf-8')
+        source_code = HtmlFile.read() 
+        components.html(source_code, height = 900,width=900)
+
+    if option=='view 2':
+        views.view_2(uploaded_file)
+        HtmlFile = open("view2.html", 'r', encoding='utf-8')
+        source_code = HtmlFile.read() 
+        components.html(source_code, height = 900,width=900)
---
+++ Visualizer/views.py
@@ -0,0 +1,178 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from pyvis.network import Network
+import pandas as pd
+import streamlit as st
+import textwrap
+
+## this file contains code for different visualzation/ views of the LePa visualizer --> aka model
+
+options = {
+   "nodes": {
+     "borderWidth": 3,
+     "borderWidthSelected": 6,
+     "shadow": {
+       "enabled": True,
+       "color": "white",
+       "size": 9,
+       "x": -1,
+       "y": -2
+     },
+     "shapeProperties": {
+       "borderRadius": 4
+     },
+     "size": 29
+   },
+   "edges": {
+     "color": {
+       "inherit": True
+     },
+     "dashes": True,
+     "font": {
+       "strokeWidth": 5
+     },
+     "hoverWidth": 3.2,
+     "scaling": {
+       "label": {
+         "min": 24
+       }
+     },
+     "selfReference": {
+       "angle": 0.7853981633974483
+     },
+     "smooth": {
+       "roundness": 0.7
+     }
+   },
+   "interaction": {
+     "hideEdgesOnZoom": True,
+     "hover": True,
+     "keyboard": {
+       "enabled": True
+     },
+     "multiselect": True,
+     "navigationButtons": True
+   },
+   "manipulation": {
+     "enabled": True
+   },
+   "physics": {
+     "minVelocity": 0.75
+   }
+ }
+#########################################################################
+def viewAll(uploaded_file):
+  df = pd.read_csv(uploaded_file)
+  # fill empty cells in specifc column with nil
+  df["Alternative"].fillna("nil", inplace = True) 
+  df_id = df['ID']
+  df_title = df['Title']
+  df_alt = df['Alternative']
+  df_tURL = df['targetUrl']
+  df_type = df['Type']
+  df_isPartOf = df['isPartOf']
+  df_assesses = df['assesses']
+  df_requires = df['requires']
+
+  data_ER = zip(df_id, df_title, df_alt, df_tURL, df_type, df_isPartOf, df_assesses, df_requires) # making tuples  
+  G = nx.DiGraph()
+  for d in data_ER:
+    d_id = d[0]
+    d_title = d[1]
+    d_type = d[4]
+    d_alt = d[2]
+    if(d_type=="aER"):
+      G.add_node(d_id, label = d_title, shape="box", title=d_alt)
+    elif(d_type == "rER"):
+      G.add_node(d_id, label = d_title, shape="triangle", title=d_alt) 
+    elif(d_type == "iER"):
+      G.add_node(d_id, label = d_title, shape="circle", title=d_alt)
+    else:
+      G.add_node(d_id, label = d_title, title=d_alt)
+        
+    ## relationship assesses:
+    d_assesses = d[6]
+    data_assess = zip(df_id)
+    for d1 in data_assess:
+      d1_id = d1[0]
+      if(d_assesses == d1_id):
+        G.add_edge(d_id, d1_id, color="red")
+        
+    ## relationship requires:
+    d_requires = d[7]
+    data_req = zip(df_id)
+    for d2 in data_req:
+      d2_id = d2[0]
+      if(d_requires == d2_id):
+        #use label to label the edges
+        G.add_edge( d2_id, d_id, weight = 5)
+
+    ## relationship isPartOf:
+    d_isPartOf = d[5]
+    data_isPartOf = zip(df_id)
+    for d3 in data_isPartOf:
+      d3_id = d3[0]
+      if(d_isPartOf == d3_id):     
+        G.add_edge( d3_id,d_id, color = "green")
+
+  G2 = Network(height="800px", width="100%", bgcolor="#222222", font_color="white", notebook=True,heading='', directed=True)
+  G2.from_nx(G)
+  G2.options = options
+  for node in G2.nodes:
+    id_string = node["label"]
+    width = 10
+    wrapped_strings = textwrap.wrap(id_string, width)
+    wrapped_id =""; 
+    for line in wrapped_strings:
+      wrapped_id = textwrap.fill(id_string, width)
+    node["label"] = wrapped_id
+    
+  G2.show('viewAll.html')
+
+########################################################################    
+def view_2(uploaded_file):
+  df = pd.read_csv(uploaded_file)
+  # fill empty cells in specifc column with nil
+  df["Alternative"].fillna("nil", inplace = True) 
+  df_id = df['ID']
+  df_title = df['Title']
+  df_alt = df['Alternative']
+  df_tURL = df['targetUrl']
+  df_type = df['Type']
+  df_isPartOf = df['isPartOf']
+  df_assesses = df['assesses']
+  df_requires = df['requires']
+    
+  data_ER = zip(df_id, df_title, df_alt, df_tURL, df_type, df_isPartOf, df_assesses, df_requires) # making tuples  
+  G = nx.DiGraph()
+  for d in data_ER:  
+    d_id = d[0]
+    d_title = d[1]
+    d_type = d[4]
+    d_alt = d[2]
+    if(d_type=="aER"):
+      G.add_node(d_id, label = d_title, shape="box", title=d_alt)
+    elif(d_type == "rER"):
+      G.add_node(d_id, label = d_title, shape="triangle", title=d_alt) 
+        
+    ## relationship assesses:
+    d_assesses = d[6]
+    data_assess = zip(df_id)
+    for d1 in data_assess:
+      d1_id = d1[0]
+      if(d_assesses == d1_id):
+        G.add_edge(d_id, d1_id, color="red")
+
+  G2 = Network(height="800px", width="100%", bgcolor="#222222", font_color="white", notebook=True,heading='', directed=True)
+  G2.from_nx(G)
+  G2.options = options
+  for node in G2.nodes:
+    id_string = node["label"]
+    width = 10
+    wrapped_strings = textwrap.wrap(id_string, width)
+    wrapped_id =""; 
+    for line in wrapped_strings:
+      wrapped_id = textwrap.fill(id_string, width)
+    node["label"] = wrapped_id
+    
+    G2.show('view2.html') 
