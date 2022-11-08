b93e7eda3f2a690cae9191ff3d7c2f1b588530ec
ElmiraOn <elmiraonagh@yahoo.com>
2022-11-07 22:35:07 -0500

    replaced side bar with horizontal option expander

--- Visualizer/VisualizerApp.py
+++ Visualizer/VisualizerApp.py
@@ -10,36 +10,36 @@ import requests
 
 
 #### this file contains code for streamlit deployment
-st.set_page_config(layout="wide")
+st.set_page_config(page_title = "LePa Visualizer", layout="wide", initial_sidebar_state="collapsed")
+
 st.title('LePa: Learning Path Project')
 # st.sidebar.title('Choose your view')
 # option=st.sidebar.selectbox('select graph',('whole LePa','view 2', 'view 3'))
 # physics=st.sidebar.checkbox('add physics interactivity?')
-demo = st.checkbox("use demo file")
+# demo = st.checkbox("use demo file")
 
-# physics=st.sidebar.checkbox('add physics interactivity?')
+# option menu:
+with st.expander("Options"):
+    option=st.selectbox('select graph',('whole LePa','view 2', 'view 3'))
+    physics=st.checkbox('add physics interactivity?')
+
+demo = st.checkbox("use demo file")
 if demo: 
     # get demo file from github
     url = "https://raw.githubusercontent.com/LePa-YU/Visualizer/main/Data/demo.csv"
     res = requests.get(url, allow_redirects=True)
     with open('demo.csv','wb') as file:
         file.write(res.content)
-    # from google drive
-    # url="https://drive.google.com/file/d/1kWkw57BPSKQ-my37NvHXszcMbuKFw7B0/view?usp=sharing"
-    # url='https://drive.google.com/uc?id=' + url.split('/')[-2]
-    # df = pd.read_csv(url)
-    # df.to_csv("demo.csv")
-
     uploaded_file = "demo.csv"
 
 else:
     uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    physics = False
-    views.viewAll(uploaded_file, physics)
-    HtmlFile = open("viewAll.html", 'r', encoding='utf-8')
-    source_code = HtmlFile.read() 
-    st.components.v1.html(source_code, height=1080, scrolling=True)
+    if option == 'whole LePa':
+        views.viewAll(uploaded_file, physics)
+        HtmlFile = open("viewAll.html", 'r', encoding='utf-8')
+        source_code = HtmlFile.read() 
+        st.components.v1.html(source_code, height=1080, scrolling=True)
         # components.html(source_code, height = 900,width=900)
 
     # if option=='view 2':
