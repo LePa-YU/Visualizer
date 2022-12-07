487ee244e28df01b798ad8842f5f7c084209285b
ElmiraOn <elmiraonagh@yahoo.com>
2022-12-07 11:57:04 -0500

    added summative_assesment_only view

--- Visualizer/views.py
+++ Visualizer/views.py
@@ -26,8 +26,9 @@ def setData(df):
   df_assesses = df['assesses']
   df_requires = df['requires']
   df_comesAfter = df['comesafter']
+  df_comesAfter_aER = df["comesafter_aer"]
   global data_ER
-  data_ER = zip(df_id, df_title, df_alt, df_tURL, df_type, df_isPartOf, df_assesses, df_requires, df_comesAfter) # making tuples 
+  data_ER = zip(df_id, df_title, df_alt, df_tURL, df_type, df_isPartOf, df_assesses, df_requires, df_comesAfter, df_comesAfter_aER) # making tuples 
 
 # initial options
 physics = {
@@ -167,13 +168,13 @@ def All_ERs(physics, bg, fix):
         G.add_edge(d_id, d1_id, color= assess_edge_color)
         
     ## relationship requires:
-    # d_requires = d[7]
-    # data_req = zip(df_id)
-    # for d2 in data_req:
-    #   d2_id = d2[0]
-    #   if(d_requires == d2_id):
-    #     #use label to label the edges
-    #     G.add_edge( d2_id, d_id, weight = 5, color= requires_edge_color)
+    d_requires = d[7]
+    data_req = zip(df_id)
+    for d2 in data_req:
+      d2_id = d2[0]
+      if(d_requires == d2_id):
+        #use label to label the edges
+        G.add_edge( d2_id, d_id, weight = 5, color= requires_edge_color)
 
     ## relationship comesAfter:
     d_comesAfter = d[8]
@@ -237,34 +238,36 @@ def Course_Overview(physics, bg, fix):
 ##########################################################
 ########################################################################    
 def Summative_assessment_only(physics, bg, fix):
+  # setColors()
   G = nx.DiGraph()
-  G.add_node(1, label="A", x=0, y=0)
-  G.add_node(2, label="B", x=5, y = 0)
-  G.add_node(3, label="C",x=10, y=0)
-  # G.add_node("D")
-  G.add_edge(1, 2)
-  G.add_edge(2, 3)
-  # G.add_edge("B", "D")
-  # p={
-  #   "A":[0, 10], 
-  #   "B": [20, 10],
-  #   "C": [30, 10]
-  # }
-  # M = nx.identified_nodes(G, "A", "B", self_loops=False)
-  
-  # convert_to_pyvis(G,bg, physics)
-  G2 = Network(height="800px", width="100%", bgcolor=bg, font_color=setFontColor(bg), notebook=True,heading='', directed=True)
-  G2.from_nx(G)
-  if physics:
-    G2.height = "500px"
-    G2.show_buttons()
-  
-  G2.options.edges = edges
-  G2.options.nodes = nodes
-  G2.options.interaction = interaction
-  G2.options.manipulation = manipulation
-  G2.options.layout = layout
-  
+  for d in data_ER:
+    d_id = d[0]
+    d_title = d[1]
+    d_type = d[4]
+    d_alt = d[2]
+    d_isPartOf = d[5]
+    if(d_type=="aER"):
+      G.add_node(d_id, label = d_title, shape="box", title=d_alt, color= aER_node_color)
+    elif(d_type == "rER"):
+      G.add_node(d_id, label = d_title, shape="triangle", title=d_alt, color = rER_node_color) 
+   
+        
+    ## relationship assesses:
+    d_assesses = d[6]
+    data_assess = zip(df_id)
+    for d1 in data_assess:
+      d1_id = d1[0]
+      if(d_assesses == d1_id):
+        G.add_edge(d_id, d1_id, color= assess_edge_color)
+
+    ## relationship comesAfter:
+    d_comesAfter_aER = d[9]
+    data_ca = zip(df_id)
+    for d2 in data_ca:
+      d2_id = d2[0]
+      if(d_comesAfter_aER == d2_id):
+        #use label to label the edges
+        G.add_edge( d2_id, d_id, weight = 5, color= requires_edge_color)
+        
   convert_to_pyvis(G,bg, physics, fix)
- 
   
