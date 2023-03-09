f5cd9ee1de8f8ce5ea1cc3d499661f02588e9431
ElmiraOn <elmiraonagh@yahoo.com>
2023-03-08 23:52:58 -0500

    fixed a bug that fixing the previous bug caused in visualizing the demo dataset

--- views.py
+++ views.py
@@ -123,11 +123,13 @@ class Views:
                 current_type = node.er_type
                 if( current_type=="aER" or current_type=="end"):
                     limit = len(self.nodeList)
-                    while(last_node.er_type == "iER" and limit <= 0):
+                    while(last_node.er_type == "iER"):
                         last_node_comesAfter = Views.__get_node_int_id(last_node.er_comesAfter)
                         if(type(last_node_comesAfter)==int):
                             last_node = Views.__Find_node(self,last_node_comesAfter)
                         limit = limit -1
+                        if(limit ==0):
+                            break
                     G.add_edge(last_node.er_id,node.er_id, weight = 5, color= self.all_colors.comesAfter_relationship_color)
 
 
