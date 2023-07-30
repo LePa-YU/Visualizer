31b864bf35aaba0c9b88236668eb5af2f206514c
ElmiraOn <elmiraonagh@yahoo.com>
2023-07-30 01:56:52 -0400

    added checks for empty title and type

--- Visualizer/validity_checker.py
+++ Visualizer/validity_checker.py
@@ -19,21 +19,27 @@ class validity_checker:
             node_id = self.df["identifier"][i]
             node_type = self.df["type"][i]
             node_title = self.df["title"][i]
-            if node_type != "start" and node_type != "end":
-                if node_type == "rER": 
-                    assesses = self.df["assesses"][i]
-                    if pd.isna(self.df.loc[i,'assesses']):
-                        self.num = self.num + 1
-                        validity_file.write(str(self.num)+". Node: " + str(node_title)+ " | ID: "+ str(node_id)+ " of type: " + str(node_type) + " Must assess an aER! \n")
-                elif node_type == "iER" or node_type == "aER": 
-                    # ca = self.df["comesAfter"][i]
-                    if pd.isna(self.df.loc[i,'comesAfter']):
-                        self.num = self.num + 1
-                        validity_file.write(str(self.num)+". Node: " + str(node_title)+ " | ID: "+ str(node_id)+ " of type: " + str(node_type) + " Must comes after an aER or iER! \n")
-                else: # node is atomic --> isPartOF
-                    if pd.isna(self.df.loc[i,'isPartOf']):
-                        self.num = self.num + 1
-                        validity_file.write(str(self.num)+". Node: " + str(node_title)+ " | ID: "+ str(node_id)+ " of type: " + str(node_type) + " Must be part of a composite ER! \n")
+            if pd.isna(self.df.loc[i,'title']) or pd.isna(self.df.loc[i,'type']):
+                self.num = self.num + 1
+                self.df["title"][i] = "Missing Title"
+                self.df.to_csv(self.file_name, index=False)
+                validity_file.write(str(self.num)+". ER is missing title or type  | ID: "+ str(node_id) + " Please delete `Missing Title` using Update a ER option\n")
+            else:
+                if node_type != "start" and node_type != "end":
+                    if node_type == "rER": 
+                        assesses = self.df["assesses"][i]
+                        if pd.isna(self.df.loc[i,'assesses']):
+                            self.num = self.num + 1
+                            validity_file.write(str(self.num)+". ER: " + str(node_title)+ " | ID: "+ str(node_id)+ " of type: " + str(node_type) + " Must assess an aER! \n")
+                    elif node_type == "iER" or node_type == "aER": 
+                        # ca = self.df["comesAfter"][i]
+                        if pd.isna(self.df.loc[i,'comesAfter']):
+                            self.num = self.num + 1
+                            validity_file.write(str(self.num)+". ER: " + str(node_title)+ " | ID: "+ str(node_id)+ " of type: " + str(node_type) + " Must comes after an aER or iER! \n")
+                    else: # node is atomic --> isPartOF
+                        if pd.isna(self.df.loc[i,'isPartOf']):
+                            self.num = self.num + 1
+                            validity_file.write(str(self.num)+". ER: " + str(node_title)+ " | ID: "+ str(node_id)+ " of type: " + str(node_type) + " Must be part of a composite ER! \n")
         validity_file.close()
     
                 
